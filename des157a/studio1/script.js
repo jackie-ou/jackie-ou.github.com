28bd88f8ae533b1625e68a166615afbf29eae3bf
jackie-ou <jhgou@ucdavis.edu>
2021-10-25 00:46:17 -0700

    splash hidden

--- des157a/studio1/script.js
+++ des157a/studio1/script.js
@@ -4,23 +4,31 @@
 
     let myForm = document.querySelector("#myForm");
     let madLib = document.querySelector("#madlib");
+    let sections = document.querySelectorAll("section");
+    let name = document.getElementById("name");
+    let story = document.getElementById("story");
+
+
+    document.querySelector('#signin').addEventListener('click', function() {
+        sections[0].className = "hidden";
+        sections[1].className = "show";
+    });
+
+
+
+
 
     myForm.addEventListener("submit", function(event){
         event.preventDefault();
         let formData = document.querySelectorAll("input[type=text]");
         processData(formData);
 
-        // clear form
-        formData = document.querySelectorAll("input[type=text]");
-        for(let x of formData){
-            i.value = "";
-        }
+        // // clear form
+        // formData = document.querySelectorAll("input[type=text]");
+        // for(let x of formData){
+        //     x.value = "";
+        // }
     });
-
-    function makeMadlib(words){
-        let myText =  `Here are the words: ${words[0]}, ${words[1]}, ${words[2]}, ${words[3]}`;
-        madLib.innerHTML = myText;
-    }
 
     function processData(formData){
         let emptyFields = false;
@@ -33,12 +41,30 @@
             } else{
                 emptyFields = true;
             }
-            if(!emptyFields){
+            if(emptyFields){
                 madLib.innerHTML = "Please fill out the fields";
             } else{
                 // update HTML
                 makeMadlib(words);
             }
         }
+    }
+
+    function makeMadlib(words){
+        let myText =  `
+        Hello professor ${words[1]} ${words[2]}. 
+        This is ${words[3]} ${words[4]} from your Dancing with (noun) class. 
+        I had a few questions to ask you regarding the course. 
+        First off, on your syllabus it says we have to bring ${words[5]} to class every Friday. 
+        This is such an odd item to have. Will we be ${words[6]} with it? 
+        Secondly, I have some concerns regarding ${words[7]} ${words[8]} from my ${words[9]} group. 
+        He hasn't improved on his ${words[10]} axle spin and keeps arguing with ${words[11]} ${words[12]}. 
+        I do not want him to bring down the group and was wondering if he could be moved to ${words[13]} ${words[14]}'s group. 
+        Lastly, I was wondering how ${words[15]} is since we havent seen her in class in a while. 
+        Thank you for your time! 
+        Please respond when you can my email is ${words[0]}@cowmail.com. 
+        See you in class!
+        `;
+        story.innerHTML = myText;
     }
 }());
