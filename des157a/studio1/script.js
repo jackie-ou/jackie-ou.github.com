5f6add463870cb336013878ccff5232d0c69b15d
jackie-ou <jhgou@ucdavis.edu>
2021-10-25 02:30:43 -0700

    studio1 draft

--- des157a/studio1/script.js
+++ des157a/studio1/script.js
@@ -5,29 +5,25 @@
     let myForm = document.querySelector("#myForm");
     let madLib = document.querySelector("#madlib");
     let sections = document.querySelectorAll("section");
-    let name = document.getElementById("name");
     let story = document.getElementById("story");
-
+    let to = document.getElementById("to");
+    let subject = document.getElementById("subject");
 
     document.querySelector('#signin').addEventListener('click', function() {
         sections[0].className = "hidden";
         sections[1].className = "show";
     });
 
-
-
-
-
     myForm.addEventListener("submit", function(event){
         event.preventDefault();
         let formData = document.querySelectorAll("input[type=text]");
         processData(formData);
 
-        // // clear form
-        // formData = document.querySelectorAll("input[type=text]");
-        // for(let x of formData){
-        //     x.value = "";
-        // }
+        // clear form
+        formData = document.querySelectorAll("input[type=text]");
+        for(let x of formData){
+            x.value = "";
+        }
     });
 
     function processData(formData){
@@ -52,19 +48,18 @@
 
     function makeMadlib(words){
         let myText =  `
-        Hello professor ${words[1]} ${words[2]}. 
-        This is ${words[3]} ${words[4]} from your Dancing with (noun) class. 
-        I had a few questions to ask you regarding the course. 
-        First off, on your syllabus it says we have to bring ${words[5]} to class every Friday. 
-        This is such an odd item to have. Will we be ${words[6]} with it? 
-        Secondly, I have some concerns regarding ${words[7]} ${words[8]} from my ${words[9]} group. 
-        He hasn't improved on his ${words[10]} axle spin and keeps arguing with ${words[11]} ${words[12]}. 
-        I do not want him to bring down the group and was wondering if he could be moved to ${words[13]} ${words[14]}'s group. 
-        Lastly, I was wondering how ${words[15]} is since we havent seen her in class in a while. 
-        Thank you for your time! 
-        Please respond when you can my email is ${words[0]}@cowmail.com. 
-        See you in class!
+        Hello professor ${words[1]}. 
+         
+        This is ${words[0]} from your Dancing with ${words[2]} class. 
+        I am not able to attend today since ${words[3]}, my ${words[4]}, ran away.
+        It is my only means of transportation so I cannot make it.
+        Can you send the Zoom recording to ${words[5]}-${words[0]}@cowmail.com. 
+         
+        Thanks!
         `;
         story.innerHTML = myText;
+
+        to.innerHTML = `To: ${words[1]}@cowmail.com`;
+        subject.innerHTML = `Subject: Skipping class because of runaway ${words[4]}`;
     }
 }());
